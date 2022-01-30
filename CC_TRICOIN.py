c32fd5b469334b5d7281195bedbe67348e947f47
Satyaki Saha <64432022+JohnBlakeSaturn@users.noreply.github.com>
2022-01-30 20:50:09 +0530

    Update CC_TRICOIN.py

--- CC_TRICOIN.py
+++ CC_TRICOIN.py
@@ -2,7 +2,7 @@ import math
 T = int(input())
 while(T):
 	n = int(input())
-		while(math.sqrt(1 + 8*n)%1 > 0):
-    		n -= 1
+	while(math.sqrt(1 + 8*n)%1 > 0):
+    	n -= 1
   	print(int((-1 + math.sqrt(1 + 8*n))/2))
   	T-=1
